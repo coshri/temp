4b48f9ac9de58827adb2ffed53e73ebdf4e5dd68
coshri <mail.oshri@gmail.com>
2017-09-25 18:47:53 +0300

    444

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("oshri");
+            Console.WriteLine("oshri 123");
         }
     }
 }
